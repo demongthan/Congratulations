58c8cf1d5d83848fe479105549da2e584de8ca4c
demongthan <nam.nguyenbku00@gmail.com>
2022-10-20 00:49:44 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Trâm';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Quốc tế phụ nữ 20-10';
+const titleCard = 'Tặng ban ggame của tui';
+const contentCard = 'Nhân ngày Phụ nữ Việt Nam, chúc bạn game của tui mãi xinh đẹp, trẻ trung, gặp nhiều may mắn trong cuộc sống, '+
+'kiếm được một anh người yêu đẹp trai giống như tui vậy, mà bạn tui lấy chồng đi nha!';
+
+const giftImage = 'tram.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
